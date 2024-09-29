246c731c213f26922ff5000b9950ee49dbac1530
carlomonterrey <carlosmonterrey17@gmail.com>
2024-09-28 22:15:16 -0400

    Refact(cart service) logic updating quantity

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -26,48 +26,39 @@ export class CartService {
 
     if (action === 'add') {
       if (index !== -1) {
-        this.add(product);
+        this.updateProductQuantity(product.id, 1); 
       } else {
-        patchState(this.productsState, {
-          selectedProducts: [...selectedProducts, { ...product, quantity: 1 }],
-        });
+        this.addNewProduct(product); 
       }
     } else {
       if (index !== -1) {
-        this.remove(product);
+        this.updateProductQuantity(product.id, -1); 
       }
     }
   }
 
-  public add(product: Product): void {
-    const selectedProducts = this.productsState().selectedProducts;
-    const existingProduct = selectedProducts.find(item => item.id === product.id);
-    
-    if (existingProduct) {
-      patchState(this.productsState, {
-        selectedProducts: selectedProducts.map(item =>
-          item.id === product.id ? { ...item, quantity: existingProduct.quantity + 1 } : item
-        ),
-      });
-    }
+  private addNewProduct(product: Product): void {
+    patchState(this.productsState, {
+      selectedProducts: [...this.productsState().selectedProducts, { ...product, quantity: 1 }],
+    });
   }
 
-  public remove(product: Product): void {
+  private updateProductQuantity(productId: number, delta: number): void {
     const selectedProducts = this.productsState().selectedProducts;
-    const index = selectedProducts.findIndex(item => item.id === product.id);
+    const existingProduct = selectedProducts.find(item => item.id === productId);
 
-    if (index !== -1) {
-      const currentProduct = selectedProducts[index];
+    if (existingProduct) {
+      const newQuantity = existingProduct.quantity + delta;
 
-      if (currentProduct.quantity > 1) {
+      if (newQuantity > 0) {
         patchState(this.productsState, {
           selectedProducts: selectedProducts.map(item =>
-            item.id === product.id ? { ...item, quantity: currentProduct.quantity - 1 } : item
+            item.id === productId ? { ...item, quantity: newQuantity } : item
           ),
         });
       } else {
         patchState(this.productsState, {
-          selectedProducts: selectedProducts.filter(item => item.id !== product.id),
+          selectedProducts: selectedProducts.filter(item => item.id !== productId),
         });
       }
     }
